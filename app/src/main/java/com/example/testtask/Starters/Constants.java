b48a3cf6294c7bb292e733b57654d889fea611d5
Ahmad Hassan <ahmadhassantanveer44@gmail.com>
2024-06-03 13:37:55 +0500

    Remove Constants.java from repository

--- app/src/main/java/com/example/testtask/Starters/Constants.java
+++
@@ -1,6 +0,0 @@
-package com.example.testtask.Starters;
-
-public class Constants {
-    public static final String BASE_URL="https://api.themoviedb.org/3/";
-    public static final String MY_KEY="ef434d29fc1348a95038da3f9ecf72c3";
-}
